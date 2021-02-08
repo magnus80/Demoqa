ebfa782d0f49de0d8c3dacd4dc2b63719a2849dd
konstantin.iryshkov <zarechny@inbox.ru>
2021-02-08 22:32:38 +0300

    Fixed DemoqaTest

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+>
+src/.gitignore,9\3\93cbdd0973c9a7b59e5cef90a94300ab95fd66e5
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+[
++src/main/java/qa/guru/demoqa/Constants.java,6\c\6c971d49c68bd5d6b00eed845c1cea9afc5a3840
+\
+,src/test/java/qa/guru/demoqa/DemoqaTest.java,e\1\e181f94ad416fd4faead41e605b4f9099a5df3c2
+`
+0src/test/java/qa/guru/selenide/SelenideTest.java,0\7\074c1c35c4e8609d306dd6bbcf6c25fbd80a8b39
+`
+0src/test/java/qa/guru/selenide/FormOneTests.java,b\9\b95171f232139a52f10d7f40e3e835483ad1a56f
--- src/test/java/qa/guru/demoqa/DemoqaTest.java
+++ src/test/java/qa/guru/demoqa/DemoqaTest.java
@@ -1,16 +1,28 @@
 package qa.guru.demoqa;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
-import static qa.guru.demoqa.Constants.*;
 
 class DemoqaTest {
+
+    private static final String FIRST_NAME = "Ivan";
+    private static final String LAST_NAME = "Ivanov";
+    private static final String USER_EMAIL = "Ivan@Ivanov.ru";
+    private static final String USER_NUMBER = "1234567890";
+    private static final String CURRENT_ADDRESS = "currentAddress";
+
+    private static final String FIO = "Ivan Ivanov";
+    private static final String GENDER = "Male";
+    private static final String DATE = "05 February,2021";
+    private static final String SUBJECT = "Biology";
+    private static final String HOBBIE = "Sports";
+    private static final String PICTURE = "guy.jpeg";
+    private static final String STATE_AND_CITY = "Uttar Pradesh Agra";
 
     @BeforeAll
     static void setUp() {
@@ -18,25 +30,22 @@ class DemoqaTest {
     }
 
     @Test
-    void ICanFillAndCheckFormTest() {
+    @Tag("demoqa")
+    void iCanFillAndCheckFormTest() {
 
         $("#firstName").setValue(FIRST_NAME);
         $("#lastName").setValue(LAST_NAME);
         $("#userEmail").setValue(USER_EMAIL);
         $("[for='gender-radio-1']").click();
         $("#userNumber").setValue(USER_NUMBER);
-        $("#dateOfBirthInput")
-                .click();
+        $("#dateOfBirthInput").click();
         $$(".react-datepicker__day")
-                .find(exactText("5"))
-                .click();
+                .find(exactText("5")).click();
         $("#subjectsContainer").click();
         $("#subjectsInput").setValue("Bio");
-        $$(".subjects-auto-complete__menu-list")
-                .first()
-                .click();
+        $(".subjects-auto-complete__menu-list").click();
         $("[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFile(new File("src/test/java/../resources/guy.jpeg"));
+        $("#uploadPicture").uploadFromClasspath("guy.jpeg");
         $("#currentAddress").setValue(CURRENT_ADDRESS);
         $("#state").scrollTo().click();
         $("#react-select-3-option-1").click();
